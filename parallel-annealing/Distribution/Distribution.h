01d708b5fefe9cfb07b3db2fe7fbce8db77dff6f
SamuilYu <sam.yusufov@gmail.com>
2020-11-20 02:24:32 +0300

    add Gibbs distribution

---
+++ parallel-annealing/Distribution/Distribution.h
@@ -0,0 +1,29 @@
+//
+// Created by samuil on 11/20/20.
+//
+
+#ifndef C_PRAC_PRAC_DISTRIBUTION_H
+#define C_PRAC_PRAC_DISTRIBUTION_H
+
+#include "math.h"
+#include "time.h"
+
+class GibbsDistribution {
+private:
+    double getProbability (double energyDifference, double temperature) {
+        return exp(-energyDifference / temperature);
+    }
+
+public:
+    GibbsDistribution() {
+        srand(time(0));
+    }
+
+    bool shouldChange (double energyDifference, double temperature) {
+        double randomDouble = random() % 1000 / 1000;
+        return randomDouble < getProbability(energyDifference, temperature);
+    }
+};
+
+
+#endif //C_PRAC_PRAC_DISTRIBUTION_H
