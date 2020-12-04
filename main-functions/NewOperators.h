f93c3acdf92712f3db660722ee49174dc42ef6db
SamuilYu <sam.yusufov@gmail.com>
2020-12-04 13:42:56 +0300

    use enable_if to throw logic error

--- main-functions/NewOperators.h
+++ main-functions/NewOperators.h
@@ -1,6 +1,7 @@
 #ifndef MAINFUNCTIONS_NEWOPERATORS_H
 #define MAINFUNCTIONS_NEWOPERATORS_H
 
+#include <sstream>
 #include "stdexcept"
 #include "MainFunctions/BaseFunction.h"
 #include "ArithmeticFunctions/Sum.h"
@@ -8,24 +9,69 @@
 #include "ArithmeticFunctions/Prod.h"
 #include "ArithmeticFunctions/Div.h"
 
-template <class A, class B>
+template<class A, class B,
+        enable_if_t<is_base_of_v<shared_ptr<BaseFunction>, A>, bool> = true,
+        enable_if_t<is_base_of_v<shared_ptr<BaseFunction>, B>, bool> = true>
 shared_ptr<BaseArithmetic> operator+(const A& first, const B& second) {
     return make_shared<Sum>(Sum(*first, *second));
 }
 
-template <class A, class B>
+template<class A, class B,
+        enable_if_t<!is_base_of_v<shared_ptr<BaseFunction>, A> ||
+                !is_base_of_v<shared_ptr<BaseFunction>, B>, bool> = true>
+shared_ptr<BaseArithmetic> operator+(const A& first, const B& second) {
+    ostringstream stream;
+    stream << "Invalid operands for operator+.";
+    throw logic_error(string(stream.str()));
+}
+
+template<class A, class B,
+        enable_if_t<is_base_of_v<shared_ptr<BaseFunction>, A>, bool> = true,
+        enable_if_t<is_base_of_v<shared_ptr<BaseFunction>, B>, bool> = true>
 shared_ptr<BaseArithmetic> operator*(const A& first, const B& second) {
     return make_shared<Prod>(Prod(*first, *second));
 }
 
-template <class A, class B>
+template<class A, class B,
+        enable_if_t<!is_base_of_v<shared_ptr<BaseFunction>, A> ||
+                    !is_base_of_v<shared_ptr<BaseFunction>, B>, bool> = true>
+shared_ptr<BaseArithmetic> operator*(const A& first, const B& second) {
+    ostringstream stream;
+    stream << "Invalid operands for operator*.";
+    throw logic_error(string(stream.str()));
+}
+
+template<class A, class B,
+        enable_if_t<is_base_of_v<shared_ptr<BaseFunction>, A>, bool> = true,
+        enable_if_t<is_base_of_v<shared_ptr<BaseFunction>, B>, bool> = true>
 shared_ptr<BaseArithmetic> operator/(const A& first, const B& second) {
     return make_shared<Div>(Div(*first, *second));
 }
 
-template <class A, class B>
+template<class A, class B,
+        enable_if_t<!is_base_of_v<shared_ptr<BaseFunction>, A> ||
+                    !is_base_of_v<shared_ptr<BaseFunction>, B>, bool> = true>
+shared_ptr<BaseArithmetic> operator/(const A& first, const B& second) {
+    ostringstream stream;
+    stream << "Invalid operands for operator/.";
+    throw logic_error(string(stream.str()));
+}
+
+template<class A, class B,
+        enable_if_t<is_base_of_v<shared_ptr<BaseFunction>, A>, bool> = true,
+        enable_if_t<is_base_of_v<shared_ptr<BaseFunction>, B>, bool> = true>
 shared_ptr<BaseArithmetic> operator-(const A& first, const B& second) {
     return make_shared<Diff>(Diff(*first, *second));
 }
+
+template<class A, class B,
+        enable_if_t<!is_base_of_v<shared_ptr<BaseFunction>, A> ||
+                    !is_base_of_v<shared_ptr<BaseFunction>, B>, bool> = true>
+shared_ptr<BaseArithmetic> operator-(const A& first, const B& second) {
+    ostringstream stream;
+    stream << "Invalid operands for operator-.";
+    throw logic_error(string(stream.str()));
+}
+
 
 #endif //MAINFUNCTIONS_NEWOPERATORS_H
